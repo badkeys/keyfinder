e2938d1a1200baa5d2dff25dc72d18f66e05de23
Hanno Böck <990588+hannob@users.noreply.github.com>
2024-07-25 21:47:51 +0200

    add option to write unparsable data to files, add multi-unescape filter

--- runci.sh
+++ runci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-ruff check --select=ALL --ignore=PTH,ANN,D,ERA,S310,T201 keyfinder
+ruff check --select=ALL --ignore=PTH,ANN,D,ERA,S310,T201,C,PLR keyfinder
 black --check --diff keyfinder
