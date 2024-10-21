f20fe9b76b3b8eee377867dbcfdad743134391df
Hanno Böck <990588+hannob@users.noreply.github.com>
2024-10-21 13:08:00 +0200

    Again disable FBT ruff rules (accidentally re-enabled...)

--- runci.sh
+++ runci.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-ruff check --select=ALL --ignore=PTH,ANN,D,ERA,S310,T201,C,PLR,S501,FIX,TD keyfinder.py
+ruff check --select=ALL --ignore=PTH,ANN,D,ERA,S310,T201,C,PLR,S501,FIX,TD,FBT keyfinder.py
