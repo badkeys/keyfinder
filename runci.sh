5281278b11037263268065138f58af89cde9c66a
Hanno Böck <990588+hannob@users.noreply.github.com>
2024-10-21 12:59:49 +0200

    disable black for now

--- runci.sh
+++ runci.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-ruff check --select=ALL --ignore=PTH,ANN,D,ERA,S310,T201,C,PLR,S501,FIX,TD,FBT keyfinder.py
-black --check --diff keyfinder.py
+ruff check --select=ALL --ignore=PTH,ANN,D,ERA,S310,T201,C,PLR,S501,FIX,TD keyfinder.py
