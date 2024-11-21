de86810d4ca0feac6bff2af1ea91d916e3aff181
Hanno Böck <990588+hannob@users.noreply.github.com>
2024-11-21 11:26:10 +0100

    typo in script name...

--- runci.sh
+++ runci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-ruff check --select=ALL --ignore=PTH,ANN,D,ERA,S310,T201,C,PLR,S501,FIX,TD,FBT,I001 keyfinder.py gitkeyfind
+ruff check --select=ALL --ignore=PTH,ANN,D,ERA,S310,T201,C,PLR,S501,FIX,TD,FBT,I001 keyfinder.py gitkeyfinder
 python -m unittest -v
